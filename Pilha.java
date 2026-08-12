a3926bfc850f353d5d89e171581951b9e39aa932
Leonardo Rafael <leonardo.bandeira.melo1@gmail.com>
2026-08-11 22:11:21 -0300

    Create Pilha.java

---
+++ Pilha.java
@@ -0,0 +1,20 @@
+package algoritmos;
+
+public class PIlha {
+	private ListaEncadeada lista = new ListaEncadeada();
+	
+	public void empilhar(int valor) {
+		lista.adicionarInicio(valor);
+	}
+	public int desempilhar() {
+		return lista.removerInicio();
+	}
+	public void imprimir() {
+		lista.imprimir();
+	}
+	public boolean estaVazia() {
+		return lista.estaVazia();
+	}
+	
+}
+
